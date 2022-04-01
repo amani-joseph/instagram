a06a1b267b58b9d1295864971938227bb5ab8dfa
amani-joseph <mr.amanijoseph@gmail.com>
2022-04-01 23:26:00 +0300

    Registered my instagram app in my project

--- instagram_prj/settings.py
+++ instagram_prj/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'instagram_app.app.InstagramAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
