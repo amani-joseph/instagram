707733496a818c2ea9cdaf3b01e98a8e9045ecc4
amani-joseph <mr.amanijoseph@gmail.com>
2022-04-03 14:31:11 +0300

    Added crispy forms styling to the registration form. Reg form works

--- instagram_prj/settings.py
+++ instagram_prj/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     "bootstrap5",
     'insta.apps.InstaConfig',
     'users_app.apps.UsersAppConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -116,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
