2e33e64b57e66c98d85968053a2c7524d878fbe4
amani-joseph <mr.amanijoseph@gmail.com>
2022-04-04 01:48:19 +0300

    Added authentication conditionals to the navbar

--- instagram_prj/urls.py
+++ instagram_prj/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('', include('insta.urls')),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users_app/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users_app/login.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users_app/logout.html'), name='logout'),
     path('admin/', admin.site.urls),
 ]
