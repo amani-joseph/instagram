ad7659d5f19e000a23a7cb338071b8c6997d19ee
amani-joseph <mr.amanijoseph@gmail.com>
2022-04-07 02:25:05 +0300

    Added tests for Profile Model

--- users_app/tests.py
+++ users_app/tests.py
@@ -1,3 +1,20 @@
+from email.mime import image
 from django.test import TestCase
-
+from .models import Profile, Post
+from django.contrib.auth.models import User
 # Create your tests here.
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User(username='Joe')
+        self.user.save()
+
+        self.profile_test = Profile(id=1,  image='default.png', bio='I really dont like tests', user=self.user)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile_test, Profile))
+
+    def test_save_profile(self):
+        self.profile_test.save_profile()
+        after = Profile.objects.all()
+        self.assertTrue(len(after) > 0)
+
